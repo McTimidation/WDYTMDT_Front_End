9c4a5b8a09fb4c676ad0383d8928d00972461c61
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-11-29 18:41:37 +0000

    first story

---
+++ app/src/app.js
@@ -0,0 +1,16 @@
+import Header from './header';
+
+
+function App() {
+    // const [ page, setPage ] = useState('Home')
+
+
+    // getData();
+    return (
+        <>
+        <Header />
+        </>
+    )
+}
+
+export default App;
